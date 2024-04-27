94bdf11fa258ae05c7c2f30c9bd3e26e6c3e6802
Julio Mafra <julioclmafra@gmail.com>
2024-04-27 00:51:05 -0300

    ciclo de vida de componentes componentdidmount - componentdidupdate

--- src/App.js
+++ src/App.js
@@ -2,36 +2,31 @@ import React, { Component } from 'react';
 
 class App extends Component {
 
-  constructor(props) {
+  constructor(props){
     super(props);
     this.state = {
-      name: 'Julio',
-      contador: 0
+      hour: new Date().toLocaleTimeString()
     };
-    
-    this.add = this.add.bind(this);
-    this.remove = this.remove.bind(this);
   }
 
-  add = () => {
-    this.state.name = "Atualiza"
-    this.state.contador++;
-    this.setState(this.state); 
+  componentDidMount(){
+
+    setInterval(() => {
+      this.setState({hour: new Date().toLocaleTimeString()});
+    }, 1000);
+
   }
 
-  remove = () => {
-    this.state.contador--;
-    this.setState(this.state); 
+  componentDidUpdate(){
+    console.log(this.state.hour);
+    console.log('atualizou');
   }
 
+ 
   render() {
     return (
       <div>
-        {this.state.name}
-        <h3><button onClick={this.remove}>-
-        </button>{this.state.contador}
-          <button onClick={this.add}>+</button>
-        </h3>
+    <p>Horário: {this.state.hour}</p>
       </div>
     );
 
