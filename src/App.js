3e25f4e5672dc426c5ae5aa506807cfb77037e24
Julio Mafra <julioclmafra@gmail.com>
2024-04-29 00:19:31 -0300

    Projeto Biscoito da Sorte

--- src/App.js
+++ src/App.js
@@ -1,67 +1,59 @@
 import React, { Component } from 'react';
+import './style.css';
 
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      form: {
-        name: '',
-        email: '',
-        password: '',
-        gender: 'male'
-      }
+      frase: ''
     }
 
-    this.showData = this.showData.bind(this);
-    this.formData = this.formData.bind(this);
+    this.frases = [
+      'Desejar algo maior pra sua vida não significa que você está insatisfeito com o que tem.',
+      'Agradeço todos os dias pela minha vida, por tudo o que conquistei até aqui. E tenho fé de que o futuro me reserva realizações ainda maiores!',
+      'Tenho uma profunda gratidão por cada lição que aprendi no passado. Afinal, foram elas que me permitiram chegar até aqui!',
+      'Lembre-se do seu passado com carinho. Afinal, é por causa dele que você está aqui hoje!',
+      'A gratidão é uma grande aliada do sucesso!',
+      'A melhor maneira de agradecer por um belo momento é desfrutá-lo plenamente.',
+      'A gratidão é o único tesouro dos humildes.',
+      'Não deixe que as pessoas te façam desistir daquilo que você mais quer na vida. Acredite. Lute. Conquiste. E acima de tudo, seja feliz!',
+      'Toda ação humana, quer se torne positiva ou negativa, precisa depender de motivação.',
+      'Conserve os olhos fixos num ideal sublime e lute sempre pelo que deseja, pois só os fracos desistem e só quem luta é digno de vida.'
+    ];
+
+    this.changeFrase = this.changeFrase.bind(this);
+
   }
 
-  showData(event) {
-    event.preventDefault();
-    const { name, email, password, gender } = this.state.form;
-
-    if (name !== '' && email !== '' && password !== '') {
-      alert(`Nome: ${name} \nEmail: ${email} \nSenha: ${password}\nSexo: ${gender}`);
-      this.setState({ error: '' });
-      return;
-    }
-
-    this.setState({ error: 'Preencha todos os campos!' });
-
+  changeFrase() {
+    this.setState({ frase: this.frases[Math.floor(Math.random() * (this.frases).length)] });
   }
 
-  formData(element) {
-    let form = this.state.form;
-    form[element.target.name] = element.target.value;
-    this.setState({ form: form });
+
+
+
+  render() {
+    return (
+      <div className='container'>
+        <img src={require('./assets/biscoito.png')} className='img' />
+        <ButtonBiscoito name='Abrir Biscoito' actionButton={this.changeFrase} />
+        <h3 className='texto-frase'>{this.state.frase}</h3>
+      </div>
+    );
+
   }
+}
+
+class ButtonBiscoito extends Component {
 
 
   render() {
     return (
       <div>
-        <h1>Novo usuário</h1>
-
-        <form onSubmit={this.showData}>
-          {this.state.error && <p>{this.state.error}</p>}
-          <label>Nome:</label>
-          <input type="text" name="name" value={this.state.form.name} onChange={this.formData} /><br></br>
-          <label>Email:</label>
-          <input type="email" name="email" value={this.state.form.email} onChange={this.formData} /><br></br>
-          <label>Senha:</label>
-          <input type="password" name="password" value={this.state.form.password} onChange={this.formData} /><br></br>
-          <select name="gender" value={this.state.form.gender} onChange={this.formData} >
-            <option value="male">Masculino</option>
-            <option value="female">Feminino</option>
-          </select><br></br>
-
-
-          <button type="submit">+ Cadastrar</button>
-        </form>
+        <button onClick={this.props.actionButton}>{this.props.name}</button>
       </div>
-    );
-
+    )
   }
 }
 
