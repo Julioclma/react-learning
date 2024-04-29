5930597915607fbca302a18190a3f0adf2a32f1b
Julio Mafra <julioclmafra@gmail.com>
2024-04-28 23:28:54 -0300

    manipulando formulários, centralizando todo o form em uma unica função

--- src/App.js
+++ src/App.js
@@ -5,27 +5,36 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      email: '',
-      password: '',
-      error: ''
+      form: {
+        name: '',
+        email: '',
+        password: '',
+        gender: 'male'
+      }
     }
 
     this.showData = this.showData.bind(this);
+    this.formData = this.formData.bind(this);
   }
 
   showData(event) {
     event.preventDefault();
-    const { name, email, password } = this.state;
+    const { name, email, password, gender } = this.state.form;
 
-    if(name !== '' && email !== '' && password !== ''){
-      alert(`Nome: ${name} \nEmail: ${email} \nSenha: ${password}`);
-      this.setState({error: ''});
+    if (name !== '' && email !== '' && password !== '') {
+      alert(`Nome: ${name} \nEmail: ${email} \nSenha: ${password}\nSexo: ${gender}`);
+      this.setState({ error: '' });
       return;
     }
 
-    this.setState({error: 'Preencha todos os campos!'});
+    this.setState({ error: 'Preencha todos os campos!' });
 
+  }
+
+  formData(element) {
+    let form = this.state.form;
+    form[element.target.name] = element.target.value;
+    this.setState({ form: form });
   }
 
 
@@ -37,11 +46,17 @@ class App extends Component {
         <form onSubmit={this.showData}>
           {this.state.error && <p>{this.state.error}</p>}
           <label>Nome:</label>
-          <input type="text" name="name" value={this.state.name} onChange={(element) => this.setState({ name: element.target.value })} /><br></br>
+          <input type="text" name="name" value={this.state.form.name} onChange={this.formData} /><br></br>
           <label>Email:</label>
-          <input type="email" name="email" value={this.state.email} onChange={(element) => this.setState({ email: element.target.value })} /><br></br>
+          <input type="email" name="email" value={this.state.form.email} onChange={this.formData} /><br></br>
           <label>Senha:</label>
-          <input type="password" name="password" value={this.state.password} onChange={(element) => this.setState({ password: element.target.value })} /><br></br>
+          <input type="password" name="password" value={this.state.form.password} onChange={this.formData} /><br></br>
+          <select name="gender" value={this.state.form.gender} onChange={this.formData} >
+            <option value="male">Masculino</option>
+            <option value="female">Feminino</option>
+          </select><br></br>
+
+
           <button type="submit">+ Cadastrar</button>
         </form>
       </div>
