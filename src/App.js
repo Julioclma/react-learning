6290c27a9e18dff27ab054ecf6d606a1f366cb7a
Julio Mafra <julioclmafra@gmail.com>
2024-04-30 02:00:31 -0300

    Hooks - UseEffect

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 function App() {
 
-  let [contator, setContator] = useState(0);
+  const [tarefas, setTarefas] = useState([]);
 
+  const [tarefa, setTarefa] = useState('');
+
+
+  useEffect(() => {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+  }, [tarefas]);
+
+
+
+  function handleAdd() {
+    setTarefas([...tarefas, tarefa]);
+    setTarefa('');
+  }
 
 
   return (
     <div>
-      <h2>{contator}</h2>
-      <button type="button" onClick={() => setContator(contator - 1)}>+ Diminui</button>
-      <button type="button" onClick={() => setContator(contator + 1)}>+ Aumenta</button>
+      <ul>
+        {tarefas.map(tarefa => (
+          <li key={tarefa}>{tarefa}</li>
+        ))
+        }
+      </ul>
+      <input type="text" name="tarefa" value={tarefa} onChange={element => setTarefa(element.target.value)} />
+      <button type="button" onClick={handleAdd}>Adicionar</button>
     </div>
   );
 
